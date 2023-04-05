2a6b88d2e6605b1ac2fc1f6340f61331fb6b9252
Sam_x_developer <129406748+Samdaveloper@users.noreply.github.com>
2023-04-05 22:36:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Noobxcoders/Music")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Noobxcoders")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Noobxcoders")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/king_x_network")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/maitri_chi_duniya")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
